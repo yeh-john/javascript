02a1443c8385c585be475e84a9517469d162c219
John <88310061+yeh-john@users.noreply.github.com>
2022-02-13 00:41:00 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,124 @@
+function add()
+{
+    // Const Romaji
+
+    // あ行
+    const a = 'あ';
+    const i = 'い';
+    const u = 'う';
+    const e = 'え';
+    const o = 'お';
+    // か行
+    const ka = 'か';
+    const ki = 'き';
+    const ku = 'く';
+    const ke = 'け';
+    const ko = 'こ';
+    // さ行
+    const sa = 'さ';
+    const si = 'し';
+    const su = 'す';
+    const se = 'せ';
+    const so = 'そ';
+    // た行
+    const ta = 'た';
+    const ti = 'ち';
+    const tu = 'つ';
+    const te = 'て';
+    const to = 'と';
+    // な行
+    const na = 'な';
+    const ni = 'に';
+    const nu = 'ぬ';
+    const ne = 'ね';
+    const no = 'の';
+    // は行
+    const ha = 'は';
+    const hi = 'ひ';
+    const hu = 'ふ';
+    const fu = 'ふ';
+    const he = 'へ';
+    const ho = 'ほ';
+    // ま行
+    const ma = 'ま';
+    const mi = 'み';
+    const mu = 'む';
+    const me = 'め';
+    const mo = 'も';
+    // や行
+    const ya = 'や';
+    const yu = 'ゆ';
+    const yo = 'よ';
+    // ら行
+    const ra = 'ら';
+    const ri = 'り';
+    const ru = 'る';
+    const re = 'れ';
+    const ro = 'ろ';
+    // わ行
+    const wa = 'わ';
+    const wo = 'を';
+    const nn = 'ん';
+
+    // Exception Romaji
+
+    // 小さい文字
+    const xa = 'ぁ';
+    const xi = 'ぃ';
+    const xu = 'ぅ';
+    const xe = 'ぇ';
+    const xo = 'ぉ';
+    const xtu = 'っ';
+    const xya = 'ゃ';
+    const xyu = 'ゅ';
+    const xyo = 'ょ';
+    // が行
+    const ga = 'が';
+    const gi = 'ぎ';
+    const gu = 'ぐ';
+    const ge = 'げ';
+    const go = 'ご';
+    // ざ行
+    const za = 'ざ';
+    const zi = 'じ';
+    const zu = 'ず';
+    const ze = 'ぜ';
+    const zo = 'ぞ';
+    // だ行
+    const da = 'だ';
+    const di = 'ぢ';
+    const du = 'づ';
+    const de = 'で';
+    const doo = 'ど';
+    // ば行
+    const ba = 'ば';
+    const bi = 'び';
+    const bu = 'ぶ';
+    const be = 'べ';
+    const bo = 'ぼ';
+    // ぱ行
+    const pa = 'ぱ';
+    const pi = 'ぴ';
+    const pu = 'ぷ';
+    const pe = 'ぺ';
+    const po = 'ぽ';
+
+
+/*
+    // Start calculate
+    const need_stake = parseFloat(goal)/parseFloat(apr)*365/parseInt(input_day);
+
+    // Result info
+    const result = 'You need to stake ';
+    const result1 = need_stake;
+    const result2 = 'Can get '+goal+coin+' reward in '+input_day+'days';
+
+    // Result
+    document.write('<h2>'+result+'<font color="red">'+result1+'</font>'+coin+'</h2>');
+    document.write('<h2>'+result2+'</h2><br>');
+    document.write('<form action="./index.html"><input type="submit" value="Caluculate again" /></form>')
+*/
+
+
+
+}
